09a35025c4c76bac2df2d678875ffbc0cbc60347
Kenya Guzman <kenyaguzmanp@gmail.com>
2017-10-31 19:30:26 -0300

    add the new images to carousel Careers

--- public_html/javascript/mainjs.js
+++ public_html/javascript/mainjs.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    console.log("ready!");
+//    console.log("ready!");
     
     //Carousel
     //Function to animate slider captions 
@@ -43,12 +43,12 @@ $(document).ready(function () {
     var navBarTop = $("#navcon").height();
     $(".navBotton").on("click", function () {
         var idSelected = $(this).attr("id");
-        console.log("id selected: " + idSelected);
+//        console.log("id selected: " + idSelected);
         var sectionFromId = idSelected.slice(-1);
         $('#dropDownMenu' + sectionFromId).addClass('animated fadeIn');
         if (windowWidth >= 768) {
-            console.log("le has dado click a wha we are");
-            console.log("seccion a: " + sectionFromId);
+//            console.log("le has dado click a wha we are");
+//            console.log("seccion a: " + sectionFromId);
             $('html,body').animate({
                 scrollTop: $("#section" + sectionFromId).offset().top - navBarTop},
                     'slow');
@@ -60,24 +60,24 @@ $(document).ready(function () {
     addClassToNavBar(windowWidth);
 
     $(window).resize(function () {
-        console.log("rezise the window!");
+//        console.log("rezise the window!");
         windowWidth = $(window).width();
         addClassToNavBar(windowWidth);
     });
 
     function addClassToNavBar(windowWidth) {
         navBarTop = $("#navcon").height();
-        console.log("alcontenttop " + navBarTop);
+//        console.log("alcontenttop " + navBarTop);
         $("#all-content").css("margin-top", navBarTop + "px");
         if (windowWidth < 768) {
-            console.log("window en modo responsive!");
+//            console.log("window en modo responsive!");
             $("#navSec1, #navSec2, #navSec3").addClass("col-xs-4");
             $(".navBotton").on("click", function () {
                 console.log("le has dado click a wha we are");
                 var idSelected = $(this).attr("id");
-                console.log("id selected: " + idSelected);
+//                console.log("id selected: " + idSelected);
                 var sectionFromId = idSelected.slice(-1);
-                console.log("seccion a: " + sectionFromId);
+//                console.log("seccion a: " + sectionFromId);
                 $('html,body').animate({
                     scrollTop: $("#section" + sectionFromId).offset().top - navBarTop},
                         'slow');
